79caafc831910d226735816e8128c4ebf1e04d83
Areberohirwa <143403649+Areberohirwa@users.noreply.github.com>
2023-10-07 22:00:04 +0200

    functions js

--- Functions/main.js
+++ Functions/main.js
@@ -71,7 +71,7 @@ console.log("het sum is:", sum1);
 
 function calculatedivision(a, b) {
     let result = a / b;
-    
+
     return result;
 }
 
@@ -92,7 +92,7 @@ showarrayofnumbers();
 // opdr 6
 
 function showarrayofbrands() {
-    let brands = ["Tesla", "Volkswagen", "Mercedes", "", "Toyota"]
+    let brands = ["Tesla", "Volkswagen", "Mercedes", "Peugeot", "Toyota"]
 
     console.log("De beste auto merk zijn;", brands)
 }
@@ -101,6 +101,140 @@ showarrayofbrands();
 
 // opdr 7
 
-function name(params) {
-    
+let computer = 7;
+let user = 5;
+
+let input = prompt("type iets in;");
+if (input === "hoger" && computer > user) {
+    alert('goed gedaan!');
+} else if (input === 'lager' && computer < user) {
+    alert('niet goed genoeg');
+} else {
+    alert('Ok !!');
 }
+
+function showcodes() {
+    console.log(computer + user + input);
+}
+
+showcodes();
+
+// opdr 8
+
+function showMessage(Ikbenoud) {
+    console.log('Ik wil gewoon zeggen dat ' + Ikbenoud);
+}
+
+showMessage('ik te oud bent');
+
+// opdr 9
+
+function showNumbers(Num1, Num2) {
+    console.log('dit is nummer ' + Num1 + ' en nummer ' + Num2 + '.  ok !!');
+}
+
+showNumbers(1, 2);
+
+// opdr 10
+
+function showSum(sum1, sum2) {
+    let sum = x + y;
+
+    return sum;
+}
+
+let sum = calculatesum(567, 3);
+
+console.log('dit is de sum', sum);
+
+// opdr 11
+function showSum(sum1, sum2) {
+    let addit = x + y;
+
+    return addit;
+}
+
+let addit = calculatesum(56, 3);
+
+console.log('dit is de sum', addit);
+
+// opdr 12
+function concatenateStrings(message1, message2) {
+    console.log('Dit is mijn leefttijd' + ' ' + message1 + ' ' + 'mijn lengte' + ' ' + message2 + ' cm.')
+}
+
+concatenateStrings(17, 180);
+
+// opdr 13
+
+function showArray(tesla) {
+    console.log('de beste model van tesla is ' + tesla);
+}
+
+showArray('x');
+
+// opdr 14
+
+let computer_1 = 7;
+let player_1 = 5;
+
+let userinput = prompt('type in hoger of lager aub');
+function probability(prob1, prob2, prob3) {
+    console.log('dit is ' + prob1 + ' en ' + prob2 + ' en last ' + prob3);
+
+    if (userinput === 'hoger' && computer_1 > prob1 + prob2 + prob3) {
+        alert(' computer nice.')
+    } else if (userinput === 'lager' && player_1 < prob1 + prob2 + prob3) {
+        alert(' player nice.')
+    } else {
+        alert('oooh!!')
+    }
+
+}
+
+probability(3, 7, 5);
+
+// opdr 15
+
+function getSum(total1, total2) {
+    let total = a + b;
+
+    return total;
+}
+
+let addition = calculatesum(78946237, 576883);
+
+console.log('dit is de sum ', addition);
+
+// opdr 17
+
+function getDivision(div1, div2) {
+    let divide = d / e;
+
+    return divide;
+}
+
+let divi = calculatedivision(3456789, 45);
+
+console.log('dit is de aantwoord;', divi);
+
+// opdr 18
+
+function getConcatenatedStrings(quotes1, quotes2) {
+    console.log('Wat denk je ' + quotes1 + 'en aub ' + quotes2);
+}
+getConcatenatedStrings('over mij', ' bedenk iets en dan zegt het hard op.');
+
+// opdr 19
+
+function getSumOfArray(le1) {
+    let array = [1, 2, 3, 4, 5];
+
+    let result = array.reduce((a, b) => {
+        return a + b;
+    });
+
+    console.log(result);
+}
+
+getSumOfArray(0);
