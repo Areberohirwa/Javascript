4ba083ade72f52492285ab42c7dfb475c6bf3591
Areberohirwa <302418973@student.rocmondriaan.nl>
2023-12-08 12:37:02 +0100

    added fetch exercises

--- JS FETCH/main.js
+++ JS FETCH/main.js
@@ -1,17 +1,67 @@
 console.log('.........main.js is running.......');
 
 fetch('https://jsonplaceholder.org/comments/').then(function (response) {
-    console.log('response ', response);
+  console.log('response ', response);
 
-    if (response.ok) {
-     // dan halen we de json data
-     return response.text();
-    } else {
-       alert('Er is iets misgegaan met het ophalen van data!')
-    }
+  if (response.ok) {
+    // dan halen we de json data
+    return response.text();
+  } else {
+    alert('Er is iets misgegaan met het ophalen van data!')
+  }
 }).then(function (json) {
   console.log('Ontvang json Data: ', json);
 
   const data = JSON.parse(json);
   console.log(data);
 })
+
+fetch('https://pokeapi.co/api/v2/pokemon/?limit=10').then((responses) => responses.json())
+  .then((datas) => {
+    console.log('Ontavang data: ', datas);
+
+    const pokemons = datas.results;
+
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i];
+      console.log('pokemon: ', pokemon)
+      let ulList = document.querySelector('ul')
+
+      ulList.innerHTML += '<li>' + pokemon.name + '</li>';
+      ulList.innerHTML += '<li>' + pokemon.url + '</li>';
+    }
+  })
+
+fetch('https://dummyjson.com/products').then((credits) => credits.json())
+  .then((objData) => {
+    console.log('Ontvang objdata: ', objData);
+    const products = objData.products;
+
+
+    for (let counter = 0; counter < products.length; counter++) {
+      const objDatasProperty = products[counter];
+      console.log('products Zijn: ', objDatasProperty);
+      let body = document.querySelector('.container')
+      const htmlCode = displayOnWeb(objDatasProperty);
+
+      body.innerHTML += htmlCode;
+    }
+  })
+
+  function displayOnWeb(objDatasProperty) {
+    const htmlCard = `
+    <div class="card">
+    <p>${objDatasProperty.brand}</p>
+    <p>${objDatasProperty.category}</p>
+    <p>${objDatasProperty.description}</p>
+    <p>${objDatasProperty.discountPercentage}</p>
+    <p>${objDatasProperty.id}</p>
+    <p>${objDatasProperty.price}</p>
+    <p>${objDatasProperty.rating}</p>
+    <p>${objDatasProperty.stock}</p>
+    <p>${objDatasProperty.thumbnail}</p>
+    <p>${objDatasProperty.title}</p>
+</div>`;
+
+return htmlCard;
+  }
---
+++ JS FETCH/readme.md
@@ -0,0 +1,33 @@
+brand
+: 
+"Dermive"
+category
+: 
+"skincare"
+description
+: 
+"Dermive Oil Free Moisturizer with SPF 20 is specifically formulated with ceramides, hyaluronic acid & sunscreen."
+discountPercentage
+: 
+13.1
+id
+: 
+18
+images
+: 
+(5) ['https://i.dummyjson.com/data/products/18/1.jpg', 'https://i.dummyjson.com/data/products/18/2.jpg', 'https://i.dummyjson.com/data/products/18/3.jpg', 'https://i.dummyjson.com/data/products/18/4.jpg', 'https://i.dummyjson.com/data/products/18/thumbnail.jpg']
+price
+: 
+40
+rating
+: 
+4.56
+stock
+: 
+88
+thumbnail
+: 
+"https://i.dummyjson.com/data/products/18/thumbnail.jpg"
+title
+: 
+"Oil Free Moisturizer 100ml"
